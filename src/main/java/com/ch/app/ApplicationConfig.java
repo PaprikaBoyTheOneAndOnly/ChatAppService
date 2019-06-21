85d6932412e89e131a6dde0196503e0ddffd4eb3
Jan Hasler <jan.hasler@axa-winterthur.ch>
2019-06-21 17:28:17 +0200

    New Service created with Springboot and Websocket, old things deleted, Login Service created

--- src/main/java/com/ch/app/ApplicationConfig.java
+++ src/main/java/com/ch/app/ApplicationConfig.java
@@ -3,6 +3,7 @@ package com.ch.app;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.server.ServerHttpRequest;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.socket.WebSocketHandler;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
@@ -13,13 +14,15 @@ import java.security.Principal;
 import java.util.Map;
 import java.util.UUID;
 
+
 @Configuration
+@EnableWebMvc
 @EnableWebSocketMessageBroker
 public class ApplicationConfig implements WebSocketMessageBrokerConfigurer {
     @Override
     public void configureMessageBroker(MessageBrokerRegistry config) {
-        config.enableSimpleBroker("/setLogin", "/validateInput", "/user");
-        config.setApplicationDestinationPrefixes("/chat-app");
+        config.enableSimpleBroker("/login");
+        config.setApplicationDestinationPrefixes("/chatApp");
     }
 
     @Override
--- src/main/java/com/ch/service/LoginService.java
+++ src/main/java/com/ch/service/LoginService.java
@@ -2,16 +2,16 @@ package com.ch.service;
 
 import com.ch.model.Account;
 import com.ch.model.AccountManager;
-import org.jboss.logging.Message;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.simp.SimpMessageHeaderAccessor;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.messaging.simp.annotation.SendToUser;
 import org.springframework.stereotype.Component;
 
 import javax.inject.Inject;
 
-@Component()
+@Component
 public class LoginService {
     @Autowired
     private SimpMessagingTemplate template;
@@ -22,9 +22,10 @@ public class LoginService {
     public LoginService() {
     }
 
-    @MessageMapping("/validateLogin")
-    public void login(SimpMessageHeaderAccessor accessor, Account account) {
+    @MessageMapping("/validate")
+    public void setLogin(SimpMessageHeaderAccessor sha, Account account) {
+        System.out.println("here");
         System.out.println("Loginrequest with Account:  " + account.toString());
-        template.convertAndSendToUser(accessor.getUser().getName(), "/user/setLogin", this.manager.isValidLogin(account));
+        template.convertAndSendToUser(sha.getUser().getName(), "/setLogin", this.manager.isValidLogin(account));
     }
 }
